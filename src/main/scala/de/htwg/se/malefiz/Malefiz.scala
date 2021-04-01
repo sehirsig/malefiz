bb4e96d891e47ca30ab5f9dca213329d4d0accad
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-04-01 15:11:07 +0200

    added User name Input

--- src/main/scala/de/htwg/se/malefiz/Malefiz.scala
+++ src/main/scala/de/htwg/se/malefiz/Malefiz.scala
@@ -1,10 +1,13 @@
 package de.htwg.se.malefiz
 
 import de.htwg.se.malefiz.model.Player
+import scala.io.StdIn.readLine
 
 object Malefiz {
   def main(args: Array[String]): Unit = {
-    val student = Player("Your Name")
+    print("Enter your name: ")
+    val name = readLine();
+    val student = Player(name)
     println("Hello, " + student.name)
   }
 }
