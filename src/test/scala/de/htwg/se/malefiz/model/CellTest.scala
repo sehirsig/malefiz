10d1d96b1b56740037bbb63cbfa7f10e7745a215
Franz Gajewski <gitcount@mailo.com>
2021-04-06 15:15:36 +0200

    added cell class

---
+++ src/test/scala/de/htwg/se/malefiz/model/CellTest.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.malefiz.model
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CellTest extends WordSpec with Matchers {
+
+}
