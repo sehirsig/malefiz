371f2fc18452b02b213131ae4fe79422825c50a6
Franz Gajewski <gitcount@mailo.com>
2021-04-09 10:19:56 +0200

    finished celltest

--- src/test/scala/de/htwg/se/malefiz/model/CellTest.scala
+++ src/test/scala/de/htwg/se/malefiz/model/CellTest.scala
@@ -11,8 +11,21 @@ class CellTest extends WordSpec with Matchers {
     "have a nice String representation" in {
       cell.toString should be("  ")
     }
-    "bla" in {
-      cell.setFree() should be("O ")
+    "have cellStatus = O when set free" in {
+      cell.setFree()
+      cell.toString() should be("O ")
+    }
+    "have cellStatus = B when set blocked" in {
+      cell.setBlocked()
+      cell.toString() should be("X ")
+    }
+    "have cellStatus = O when set secure" in {
+      cell.setSecure()
+      cell.toString() should be("O ")
+    }
+    "have cellStatus = O when set to goal" in {
+      cell.setGoal()
+      cell.toString() should be("G ")
     }
   }}
 }
