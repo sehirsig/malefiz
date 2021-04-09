981d58a915523ddc424e17404a2043fa5f1f7260
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-04-09 11:26:02 +0200

    added gameboard to Malefiz.scala

--- src/main/scala/de/htwg/se/malefiz/Malefiz.scala
+++ src/main/scala/de/htwg/se/malefiz/Malefiz.scala
@@ -1,10 +1,16 @@
 package de.htwg.se.malefiz
 
-import de.htwg.se.malefiz.model.Player
+import de.htwg.se.malefiz.model.{Gameboard, Player}
 
 object Malefiz {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
     println("Hello, " + student.name)
+
+    val spielbrett = Gameboard()
+
+    spielbrett.initiating()
+
+    spielbrett.update()
   }
 }
