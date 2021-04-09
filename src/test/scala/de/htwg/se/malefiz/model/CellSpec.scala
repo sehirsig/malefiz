35f58f0dfd31d27d8e7e29595200f90c50718b7a
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-04-09 11:27:08 +0200

    CellTest -> CellSpec

---
+++ src/test/scala/de/htwg/se/malefiz/model/CellSpec.scala
@@ -0,0 +1,31 @@
+package de.htwg.se.malefiz.model
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CellSpec extends WordSpec with Matchers {
+  "A Cell" when { "new" should {
+    val cell = Cell()
+    "have a nice String representation" in {
+      cell.toString should be("  ")
+    }
+    "have cellStatus = O when set free" in {
+      cell.setFree()
+      cell.toString() should be("O ")
+    }
+    "have cellStatus = B when set blocked" in {
+      cell.setBlocked()
+      cell.toString() should be("X ")
+    }
+    "have cellStatus = O when set secure" in {
+      cell.setSecure()
+      cell.toString() should be("O ")
+    }
+    "have cellStatus = G when set to goal" in {
+      cell.setGoal()
+      cell.toString() should be("G ")
+    }
+  }}
+}
