ed10ba8e5353c99789a7e54f264e943e2a4a8515
Franz Gajewski <gitcount@mailo.com>
2021-04-22 15:12:37 +0200

    added hierachical architecture

--- src/main/scala/de/htwg/se/malefiz/Malefiz.scala
+++ src/main/scala/de/htwg/se/malefiz/Malefiz.scala
@@ -1,31 +1,36 @@
 package de.htwg.se.malefiz
 
+import de.htwg.se.malefiz.aview.TUI
 import de.htwg.se.malefiz.model.properties.Settings
 import de.htwg.se.malefiz.model.{Cell, Dice, Gameboard, Gamefigure, Player}
 
 object Malefiz {
   val set = Settings();
   var spielbrett = new Gameboard[Cell](set.xDim, set.yDim)
-  var dice = Dice()
+  val game = new TUI[Cell](spielbrett)
+
 
   def main(args: Array[String]): Unit = { //Viel Weniger Text, alles in Methoden verpacken
-    var input: String = ""
-    println(
-      """Welcome to Malefiz!
-        |Please Enter your Name:
-        |""".stripMargin)
-
-    var namee = readLine()
-
-    val student1 = Player(namee, 1)
-    println("Hello, " + student1.name)
-    println(
-      """
-        |Type anything to Start! Type Q to quit
-        |""".stripMargin)
-    while (readLine() != "Q") {
-      println("You have rolled an: " + dice.roll)
-      println(spielbrett.toString)
-    }
+    game.init
+//    var input: String = ""
+//    println(
+//      """Welcome to Malefiz!
+//        |Please Enter your Name:
+//        |""".stripMargin)
+//
+//    var namee = readLine()
+//
+//    val student1 = Player(namee, 1)
+//    println("Hello, " + student1.name)
+//    println(
+//      """
+//        |Type anything to Start! Type Q to quit
+//        |""".stripMargin)
+//    val game = new TUI[Cell](spielbrett)
+//    game.init
+//    while (readLine() != "Q") {
+//      println("You have rolled an: " + dice.roll)
+//      println(spielbrett.toString)
+//    }
   }
 }
--- src/main/scala/de/htwg/se/malefiz/aview/TUI.scala
+++ src/main/scala/de/htwg/se/malefiz/aview/TUI.scala
@@ -1,14 +1,37 @@
 package de.htwg.se.malefiz.aview
 
-import de.htwg.se.malefiz.model.{Cell, Gameboard}
+import de.htwg.se.malefiz.controller.Controller
+import de.htwg.se.malefiz.model._
 
-class TUI
-{
-  def command(input: String, brett:Gameboard[Cell]):Gameboard[Cell] = {
-    input match {
-      case "h" => {
-        println("Test")
-        brett
+case class TUI[T](gameboard: Gameboard[T]) {
+  def init(): Unit = {
+    var dice = Dice()
+    val figurDraussen = true
+    val controller = new Controller()
+
+    println(
+      """Welcome to Malefiz!
+        |Please Enter your Name:
+        |""".stripMargin)
+    var namee = scala.io.StdIn.readLine()
+
+    val student1 = Player(namee, 1)
+    println("Hello, " + student1.name)
+    println(
+      """
+        |Type anything to Start! Type Q to quit
+        |""".stripMargin)
+    while (scala.io.StdIn.readLine() != "Q") {
+      val roll = dice.roll
+      println("You have rolled an: " + roll)
+      println(gameboard.toString)
+      if(figurDraussen) {
+        for (i <- 1 to roll) {
+          val move = scala.io.StdIn.readLine()
+          move match {
+            case a => controller.moveUp()
+          }
+        }
       }
     }
   }
