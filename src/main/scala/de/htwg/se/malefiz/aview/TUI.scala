7260fcc1a23ef661a2e6f0341cfa3de00d97c378
Franz Gajewski <gitcount@mailo.com>
2021-04-23 09:55:27 +0200

    clean up3

--- src/main/scala/de/htwg/se/malefiz/aview/TUI.scala
+++ src/main/scala/de/htwg/se/malefiz/aview/TUI.scala
@@ -13,23 +13,19 @@ case class TUI(controller: Controller) extends Observer {
     var dice = Dice()
     val figurDraussen = true
 
-//    while (scala.io.StdIn.readLine() != "Q") {
-      val roll = dice.roll
-      println("You have rolled an: " + roll)
-//      println(spielbrett.toString)
-      if(figurDraussen) {
-        for (i <- 1 to roll) {
-          val move = scala.io.StdIn.readLine()
-          move match {
-            case "w" => controller.moveUp()
-            case "a" => controller.moveLeft()
-            case "s" => controller.moveDown()
-            case "d" => controller.moveRight()
-            case _ => println("invalid input")
-          }
+    val roll = dice.roll
+    println("You have rolled a: " + roll)
+    if(figurDraussen) {
+      for (i <- 1 to roll) {
+        val move = scala.io.StdIn.readLine()
+        move match {
+          case "w" => controller.moveUp()
+          case "a" => controller.moveLeft()
+          case "s" => controller.moveDown()
+          case "d" => controller.moveRight()
+          case _ => println("invalid input")
         }
-//      }
-//      println("press any key to roll again (Q to exit)")
+      }
     }
   }
 }
