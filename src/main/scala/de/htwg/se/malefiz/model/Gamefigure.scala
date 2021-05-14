73209e946362c3837b4bca17c804534565d10ae6
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-14 03:36:33 +0200

    GameFigure gets

--- src/main/scala/de/htwg/se/malefiz/model/Gamefigure.scala
+++ src/main/scala/de/htwg/se/malefiz/model/Gamefigure.scala
@@ -1,5 +1,7 @@
 package de.htwg.se.malefiz.model
 
 case class Gamefigure(number: Int, spieler:Player) {
+  def getNumber:Int = number
+  def getPlayer:Player = spieler
   override def toString:String = spieler.Playerid.toString
 }
