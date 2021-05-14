064f14d4a0fa9314909150ca6b8aa8b54d725f5f
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-14 13:13:01 +0200

    Fix PlayerState

--- src/main/scala/de/htwg/se/malefiz/controller/PlayerState.scala
+++ src/main/scala/de/htwg/se/malefiz/controller/PlayerState.scala
@@ -1,26 +1,38 @@
 package de.htwg.se.malefiz.controller
 
 trait PlayerState {
-  def nextPlayer: PlayerState
+  def nextPlayer(playerCount:Int): PlayerState
   def getCurrentPlayer: Int
 }
 
 object PlayerState1 extends PlayerState {
-  override def nextPlayer: PlayerState = PlayerState2
+  override def nextPlayer(playerCount:Int): PlayerState = PlayerState2
   override def getCurrentPlayer: Int = 1
 }
 
 object PlayerState2 extends PlayerState {
-  override def nextPlayer: PlayerState = PlayerState3
+  override def nextPlayer(playerCount:Int): PlayerState = {
+    if(playerCount < 3) {
+      PlayerState1
+    } else {
+      PlayerState3
+    }
+  }
   override def getCurrentPlayer: Int = 2
 }
 
 object PlayerState3 extends PlayerState {
-  override def nextPlayer: PlayerState = PlayerState4
+  override def nextPlayer(playerCount:Int): PlayerState = {
+    if(playerCount < 4) {
+      PlayerState1
+    } else {
+      PlayerState4
+    }
+  }
   override def getCurrentPlayer: Int = 3
 }
 
 object PlayerState4 extends PlayerState {
-  override def nextPlayer: PlayerState = PlayerState1
+  override def nextPlayer(playerCount:Int): PlayerState = PlayerState1
   override def getCurrentPlayer: Int = 4
 }
