b18798270fcb57343241e479fd97f79d7eb7e83c
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-14 03:36:22 +0200

    Dice to Singleton

--- src/main/scala/de/htwg/se/malefiz/model/Dice.scala
+++ src/main/scala/de/htwg/se/malefiz/model/Dice.scala
@@ -2,9 +2,10 @@ package de.htwg.se.malefiz.model
 
 import scala.util.Random
 
-case class Dice(){
-  def roll: Int = {
+object Dice { //Singleton
+  def diceRoll: Int = {
     val rnd = new Random()
     rnd.nextInt(6) + 1
   }
+
 }
