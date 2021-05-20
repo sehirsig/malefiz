474a2892d693f46be1ebabc2019bb88128decb6e
Franz Gajewski <gitcount@mailo.com>
2021-05-20 18:27:38 +0200

    player spec update

--- src/test/scala/de/htwg/se/malefiz/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/malefiz/model/PlayerSpec.scala
@@ -4,57 +4,19 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
 class PlayerSpec extends AnyWordSpec with Matchers {
-  "A Player 0" when { "new" should {
-    val player = Player("Your Name", 0)
-    "have a name"  in {
-      player.name should be("Your Name")
-    }
-    "should unapply" in {
-      Player.unapply(player).get should be("Your Name", 0)
-    }
-    "have a nice String representation" in {
-      player.toString should be("0 Your Name")
-    }
-  }}
-
   "A Player 1" when { "new" should {
-    val player = Player("Your Name", 1)
+    val player = Player("Your Name", 1, (14,7))
     "have a name"  in {
       player.name should be("Your Name")
     }
     "should unapply" in {
-      Player.unapply(player).get should be("Your Name", 1)
+      Player.unapply(player).get should be("Your Name", 1, (14,7))
     }
     "have a nice String representation" in {
       player.toString should be("1 Your Name")
     }
-  }}
-
-  "A Player 2" when { "new" should {
-    val player = Player("Your Name", 2)
-    "have a name"  in {
-      player.name should be("Your Name")
-    }
-    "should unapply" in {
-      Player.unapply(player).get should be("Your Name", 2)
-    }
-    "have a nice String representation" in {
-      player.toString should be("2 Your Name")
+    "have a starting position" in {
+      player.startingPos should be((14,7))
     }
   }}
-
-  "A Player 3" when { "new" should {
-    val player = Player("Your Name", 3)
-    "have a name"  in {
-      player.name should be("Your Name")
-    }
-    "should unapply" in {
-      Player.unapply(player).get should be("Your Name", 3)
-    }
-    "have a nice String representation" in {
-      player.toString should be("3 Your Name")
-    }
-  }}
-
-
 }
