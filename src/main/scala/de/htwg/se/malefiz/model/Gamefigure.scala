c3c2c851ae0d9c92bf87e6acb1e3e6e7eababd13
Franz Gajewski <gitcount@mailo.com>
2021-05-20 13:53:31 +0200

    update func for player and gamefigure

--- src/main/scala/de/htwg/se/malefiz/model/Gamefigure.scala
+++ src/main/scala/de/htwg/se/malefiz/model/Gamefigure.scala
@@ -3,5 +3,8 @@ package de.htwg.se.malefiz.model
 case class Gamefigure(number: Int, player: Player, pos: (Int, Int)) {
   def getNumber:Int = number
   def getPlayer:Player = player
+  def updatePos(pos: (Int, Int)): Gamefigure = {
+    copy(pos = pos)
+  }
   override def toString:String = player.Playerid.toString
 }
--- src/main/scala/de/htwg/se/malefiz/model/Player.scala
+++ src/main/scala/de/htwg/se/malefiz/model/Player.scala
@@ -2,6 +2,10 @@ package de.htwg.se.malefiz.model
 
 case class Player(name: String, Playerid: Int, startingPos: (Int, Int)) {
    val cell = Cell(Playerid.toString + " ")
-
+   var players = Array[Gamefigure]()
+   def addFigure(): Unit = {
+      val figure = Gamefigure(1, Player.this, startingPos)
+      players :+ figure
+   }
    override def toString: String = Playerid + " " + name
 }
