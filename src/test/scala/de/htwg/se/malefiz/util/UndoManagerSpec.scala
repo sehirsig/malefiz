28b549473ea733a268d57d84ff228819da45f5a4
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-21 14:20:01 +0200

    More Coverage

---
+++ src/test/scala/de/htwg/se/malefiz/util/UndoManagerSpec.scala
@@ -0,0 +1,50 @@
+package de.htwg.se.malefiz.util
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+
+
+class UndoManagerSpec  extends AnyWordSpec with Matchers {
+  "A UndoManager " should {
+    val undoManager = new UndoManager
+    "do a step in doStep" in {
+      val cmd = new testCommand
+      undoManager.doStep(cmd)
+      cmd.saved should be(1)
+    }
+    "do a undo in undoStep" in {
+      val cmd = new testCommand
+      undoManager.doStep(cmd)
+      undoManager.undoStep
+      cmd.saved should be(0)
+    }
+    "do a redo in redoStep" in {
+      val cmd = new testCommand
+      undoManager.doStep(cmd)
+      undoManager.undoStep
+      undoManager.redoStep
+      cmd.saved should be(1)
+    }
+    "has working Stacks" in {
+      val cmd = new testCommand
+      undoManager.doStep(cmd)
+      undoManager.doStep(cmd)
+      undoManager.doStep(cmd)
+      undoManager.doStep(cmd)
+      undoManager.doStep(cmd)
+      undoManager.doStep(cmd)
+      undoManager.undoStep
+      undoManager.undoStep
+      undoManager.undoStep
+      undoManager.undoStep
+      undoManager.undoStep
+      undoManager.undoStep
+      undoManager.redoStep
+      undoManager.redoStep
+      undoManager.redoStep
+      undoManager.redoStep
+      cmd.saved should be(4)
+    }
+  }
+}
