b00f055ea14d0b04757ffa987dab7d9233cc0b34
Franz Gajewski <gitcount@mailo.com>
2021-06-04 10:28:43 +0200

    tuispec

--- src/test/scala/de/htwg/se/malefiz/aview/TUISpec.scala
+++ src/test/scala/de/htwg/se/malefiz/aview/TUISpec.scala
@@ -1,23 +1,39 @@
-//package de.htwg.se.malefiz.aview
-//
-//import org.scalatest.wordspec.AnyWordSpec
-//import org.scalatest.matchers.should.Matchers
-//import de.htwg.se.malefiz.controller.Controller
-//import de.htwg.se.malefiz.controller.GameStatus._
-//import de.htwg.se.malefiz.model.Gameboard
-//import de.htwg.se.malefiz.model.properties.Settings
-//
-//class TUISpec extends AnyWordSpec with Matchers {
-//  "A Malefiz Tui" should {
-//    val controller =  Controller(new Gameboard(Settings().xDim, Settings().yDim))
-//    val tui =  TUI(controller)
+package de.htwg.se.malefiz.aview
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import de.htwg.se.malefiz.controller.Controller
+import de.htwg.se.malefiz.controller.GameStatus._
+import de.htwg.se.malefiz.model.Gameboard
+import de.htwg.se.malefiz.model.properties.Settings
+
+class TUISpec extends AnyWordSpec with Matchers {
+  "A Malefiz Tui" should {
+    val controller =  Controller(new Gameboard(Settings().xDim, Settings().yDim))
+    val tui =  TUI(controller)
+    "should be initialy idle" in {
+      tui.currentState should be(IdleTUIState)
+    }
+    "should process an input" in {
+      tui.processing("p")
+      tui.currentState should be(PlayerNameState)
+      tui.processing("xy")
+      tui.currentState should be(IdleTUIState)
+    }
+    "should react to events" in {
+      controller.selectFigure(1)
+      tui.currentState should be(MovingTUIState)
+    }
+  }
 //    "do nothing on input 'welcomeMessage'" in {
 //      tui.processing("welcomeMessage")
 //    }
 //    "add players on input 'p'" in {
 //      tui.processing("p")
+//      tui.processing("xy")
 //      tui.processing("p")
-//      tui.currentState should be(Ready1TUIState)
+//      tui.processing("yz")
+//      tui.currentState should be(IdleTUIState)
 //    }
 //    "start the game on input 'start'" in {
 //      tui.processing("s")
@@ -42,4 +58,4 @@
 //      tui.processing("krgr")
 //    }
 //  }
-//}
+}
