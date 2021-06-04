94f4a4258b3e61d5c53e9ceab3cb333a2dda5965
Franz Gajewski <gitcount@mailo.com>
2021-06-04 11:06:11 +0200

    com

---
+++ src/test/scala/de/htwg/se/malefiz/controller/MoveCommandSpec.scala
@@ -0,0 +1,14 @@
+package de.htwg.se.malefiz.controller
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class MoveCommandSpec extends AnyWordSpec with Matchers {
+  "A MoveCommand" when {
+    "should" should {
+      var playerStatus: PlayerState = PlayerState1
+      "iterate correctly with 2 Players" in {
+      }
+    }
+  }
+}
