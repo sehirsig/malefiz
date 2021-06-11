e2eac1d24069a3b2ac4edb5813a3fd8ebf2d83bd
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-06-11 02:41:47 +0200

    Components

--- src/main/scala/de/htwg/se/malefiz/controller/controllerComponent/controllerMockImpl/Controller.scala
+++ src/main/scala/de/htwg/se/malefiz/controller/controllerComponent/controllerMockImpl/Controller.scala
@@ -1,20 +1,23 @@
 package de.htwg.se.malefiz.controller.controllerComponent.controllerMockImpl
 
 import de.htwg.se.malefiz.controller.controllerComponent.ControllerInterface
+import de.htwg.se.malefiz.model.gameboardComponent.GameboardInterface
 
-class Controller extends ControllerInterface{
-  def resetGame(): Unit = {}
-  def selectFigure(x: Int): Unit = {}
-  def addPlayer(): Unit = {}
-  def addPlayerName(name: String): Unit = {}
-  def startGame(): Unit = {}
-  def boardToString(): String = ""
-  def rollDice(): Int = 0
-  def checkWin(): Unit = {}
-  def setBlockStrategy(blockStrategy: String): Unit = {}
-  def move(input: String, figurenum: Int): Unit = {}
-  def emptyMan: Unit = {}
-  def undoAll: Unit = {}
-  def undo: Unit = {}
-  def redo: Unit = {}
+import scala.swing.Publisher
+
+case class Controller(var gameboard: GameboardInterface) extends ControllerInterface with Publisher{
+  override def resetGame(): Unit = {}
+  override def selectFigure(x: Int): Unit = {}
+  override def addPlayer(): Unit = {}
+  override def addPlayerName(name: String): Unit = {}
+  override def startGame(): Unit = {}
+  override def boardToString(): String = ""
+  override def rollDice(): Int = 0
+  override def checkWin(): Unit = {}
+  override def setBlockStrategy(blockStrategy: String): Unit = {}
+  override def move(input: String, figurenum: Int): Unit = {}
+  override def emptyMan: Unit = {}
+  override def undoAll: Unit = {}
+  override def undo: Unit = {}
+  override def redo: Unit = {}
 }
