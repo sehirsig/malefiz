58cadeb7aa4e326980db8835628e9b67e0530249
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-06-17 20:43:30 +0200

    fileio

--- src/main/scala/de/htwg/se/malefiz/MalefizModule.scala
+++ src/main/scala/de/htwg/se/malefiz/MalefizModule.scala
@@ -4,8 +4,10 @@ import com.google.inject.AbstractModule
 import com.google.inject.name.Names
 import net.codingwell.scalaguice.ScalaModule
 import de.htwg.se.malefiz.controller.controllerComponent._
+import de.htwg.se.malefiz.model.fileIoComponent.FileIOInterface
 import de.htwg.se.malefiz.model.gameboardComponent.GameboardInterface
 import de.htwg.se.malefiz.model.gameboardComponent.gameboardAdvancedImpl.Gameboard
+import de.htwg.se.malefiz.model.fileIoComponent._
 
 class MalefizModule extends AbstractModule with ScalaModule {
 
@@ -17,5 +19,7 @@ class MalefizModule extends AbstractModule with ScalaModule {
     bindConstant().annotatedWith(Names.named("DefaultSizeY")).to(defaultSizeY)
     bind[ControllerInterface].to[controllerBaseImpl.Controller]
     bind[GameboardInterface].toInstance(new Gameboard(defaultSizeX,defaultSizeY))
+
+    bind[FileIOInterface].to[fileIoJsonImpl.FileIO]
   }
 }
