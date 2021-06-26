024615765984d9fb3ccc7c5a27249e5d844345ff
franzgajewski <gitcount@mailo.com>
2021-06-26 20:17:21 +0200

    comment to english player component

--- src/main/scala/de/htwg/se/malefiz/model/playerComponent/PlayerBuilder.scala
+++ src/main/scala/de/htwg/se/malefiz/model/playerComponent/PlayerBuilder.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.malefiz.model.playerComponent
 
-/** Builder-Pattern für unsere Spieler-Klasse.
+/** Builder pattern for player class.
  *
  *  @author sehirsig & franzgajewski
  */
@@ -11,30 +11,30 @@ trait PlayerBuilder {
   def build(): Player
 }
 
-/** Implementierung des Interfaces */
+/** Implement of the interface. */
 case class PlayerBuilderImp() extends PlayerBuilder {
   var name: String = ""
   var id: Int = 0
   var pos:(Int, Int) = (0,0)
 
-  /** Name des Spielers setzen */
+  /** Set player name. */
   override def setName(nameNew: String): PlayerBuilder = {
     this.name = nameNew
     this
   }
 
-  /** ID des Spielers setzen */
+  /** Set player ID. */
   override def setID(idNew: Int): PlayerBuilder = {
     this.id = idNew
     this
   }
 
-  /** Startposition für die Spielfiguren des Spielers setzen */
+  /** Set starting position for game figures of the player. */
   override def setStartingPos(posNew: (Int, Int)): PlayerBuilder = {
     this.pos = posNew
     this
   }
 
-  /** Den Spieler zusammen bauen */
+  /** Build player. */
   override def build(): Player = Player(name, id, pos)
 }
